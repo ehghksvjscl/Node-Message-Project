c27403781e62504fcad4f624e0afb45a991cfa61
kim-do-hwan-sc <dhkim3@aspringcloud.com>
2021-12-09 19:04:35 +0900

    init

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
+    // https://reacttraining.com/react-router/web/example/basic
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      TODO
     </div>
   );
 }
