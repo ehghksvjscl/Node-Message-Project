fb12ef08a7b25189fc1b9001b7d86c2046989a92
Saemi_hong <smhong@aspringcloud.com>
2022-01-01 18:27:37 +0900

    feat : Add body overflowHidden

--- client/src/components/views/Link/LinkPage.js
+++ client/src/components/views/Link/LinkPage.js
@@ -2,11 +2,25 @@ import React from 'react'
 import Tiger from '../../common/Background/Tiger';
 import Number from '../../common/Background/Number';
 import PinkButton from '../../common/Buttons/PinkButton';
+import styled from 'styled-components';
+
+const StyledTitle = styled.div`
+    font-size: 18px;
+    font-weight: bold;
+    position: absolute;
+    top: 120px;
+    margin: 0 auto;
+    text-align: center;
+`
 
 function LinkPage() {
     return (
         <div className="app">
             <Number />
+                <StyledTitle>
+                   <p><span></span>님의 복주머니에 <span></span>개의 복주머니가 있습니다. </p>
+                    <p>확인은 <span>설날(2/1)</span>에 가능합니다.</p>
+                </StyledTitle>
             <Tiger />
             <PinkButton />
         </div>
