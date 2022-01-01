e81600ea0b80b6cf3131f7de941d6188a702b908
Saemi_hong <smhong@aspringcloud.com>
2022-01-01 16:19:14 +0900

    feat : Add backgroundImage component

---
+++ client/src/components/common/Background/Tiger.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tiger from "../../../assets/tiger.png"
+
+function Tiger() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tiger
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,14 @@
 // eslint-disable-next-line
 import React from 'react'
 import { FaCode } from "react-icons/fa";
+import Background from '../../common/Background/Background';
 
 function LandingPage() {
     return (
         <>
             <div className="app">
                 <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
         </>
     )
 }
