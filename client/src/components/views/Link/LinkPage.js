6b4eb06a60567a6a2df8911ed81adbaf843379e0
saemi <sstyle940104@gmail.com>
2022-01-02 16:05:48 +0900

    feat : Add name and msgNum state in LinkPage

--- client/src/components/views/Link/LinkPage.js
+++ client/src/components/views/Link/LinkPage.js
@@ -1,4 +1,4 @@
-import React,{ useEffect, useSate } from 'react'
+import React,{ useEffect, useState } from 'react'
 import Tiger from '../../common/Background/Tiger';
 import Number from '../../common/Background/Number';
 import PinkButton from '../../common/Buttons/PinkButton';
@@ -13,20 +13,31 @@ const StyledTitle = styled.div`
     margin: 0 auto;
     text-align: center;
 `
+const StyledRedSpan = styled.span`
+    color: red
+`
 
 function LinkPage({match}) {
     useEffect(() => {
         Axios.get(`http://localhost:5000/api/messages/list/${match.params.id}`)
-            .then(response=> console.log("res", response.data.messages[0].user_id.name))
-    }, [])
+            .then(response=> {
+                return
+                console.log("res", response.data.messages[0].user_id.name)
+                setName(response.data.messages[0].user_id.name)
+                setMsgNum(response.data.messages.length)
+            })
+    }, [match.params.id])
+
+    const [name, setName] = useState("")
+    const [msgNum, setMsgNum] = useState(0)
 
 
     return (
         <div className="app">
             <Number />
                 <StyledTitle>
-                   <p><span></span>님의 복주머니에 <span></span>개의 복주머니가 있습니다. </p>
-                    <p>확인은 <span>설날(2/1)</span>에 가능합니다.</p>
+                   <p><StyledRedSpan>{name}</StyledRedSpan>님의 복주머니에 <StyledRedSpan>{msgNum}</StyledRedSpan>개의 복주머니가 있습니다. </p>
+                    <p>확인은 <StyledRedSpan>설날(2/1)</StyledRedSpan>에 가능합니다.</p>
                 </StyledTitle>
             <Tiger />
             <PinkButton />
