c041ca52dd26b3802192bb1447658371a249d2d0
ehghksvjscl <ehghkswodnjs@naver.com>
2022-01-11 20:31:06 +0900

    0111

--- server/routes/users.js
+++ server/routes/users.js
@@ -50,7 +50,7 @@ router.post("/login", (req, res) => {
                     .cookie("w_auth", user.token)
                     .status(200)
                     .json({
-                        loginSuccess: true, userId: user._id
+                        loginSuccess: true, userId: user._id   
                     });
             });
         });
